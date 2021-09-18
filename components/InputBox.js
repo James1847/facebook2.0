ed3d50535073738347db31351715ec58b085a1b0
James <1536755789@qq.com>
2021-09-18 16:56:32 +0800

    add useref funciton

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,13 +1,23 @@
 import { useSession } from "next-auth/client"
 import Image from "next/image"
 import { PhotographIcon, VideoCameraIcon, EmojiHappyIcon} from "@heroicons/react/outline"
+import { useRef } from 'react'
+import { db } from "../firebase"
 
 
 function InputBox() {
     const [session] = useSession();
+    const inputRef = useRef();
+
 
     const sendPost = (e) => {
         e.preventDefault();
+        if (!inputRef.current.value) return;
+
+        db.collection('posts').push(
+            message: '1',
+            name: '张三'
+        )
     };
 
     return (
@@ -15,7 +25,7 @@ function InputBox() {
             <div className="flex space-x-4 p-4 items-center">
                 <Image className="rounded-full" src={session.user.image} width={40} height={40} layout="fixed" alt=""></Image>
                 <form className="flex flex-1">
-                    <input type="text" className="rounded-full h-12 flex-2 bg-gray-100 flex-grow px-5 focus:outline-none" placeholder={`${session.user.name} 来分享你的新鲜事吧`} />
+                    <input ref={inputRef} type="text" className="rounded-full h-12 flex-2 bg-gray-100 flex-grow px-5 focus:outline-none" placeholder={`${session.user.name} 来分享你的新鲜事吧`} />
                     <button hidden type="submit" onClick={sendPost}>发帖</button>
                 </form>
             </div>
