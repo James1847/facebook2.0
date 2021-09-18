54447736ca66d019fb1087cdec88315b2d18810d
James <1536755789@qq.com>
2021-09-18 11:11:46 +0800

    input box 2.0

--- components/InputBox.js
+++ components/InputBox.js
@@ -4,12 +4,17 @@ import Image from "next/image"
 function InputBox() {
     const [session] = useSession();
 
+    // const sendPost = (e) => {
+    //     e.preventDefault();
+    // };
+
     return (
-        <div>
-            <div>
+        <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+            <div className="flex space-x-4 p-4 items-center">
                 <Image className="rounded-full" src={session.user.image} width={40} height={40} layout="fixed" alt=""></Image>
                 <form className="flex flex-1">
-                    <input type="text" className="" placeholder="分享你的新鲜事吧" />
+                    <input type="text" className="rounded-full h-12 flex-2 bg-gray-100 flex-grow px-5 focus:outline-none" placeholder={`${session.user.name} 来分享你的新鲜事吧`} />
+                    {/* <button hidden type="submit" onClick={sendPost}>发帖</button> */}
                 </form>
             </div>
         </div>
